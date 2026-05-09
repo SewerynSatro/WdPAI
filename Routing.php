643a0e89a79c527ba4c83fd230afcca1564eddc8
SewerynSatro <sewerynsatro@gmail.com>
2026-05-09 17:55:11 +0200

    Add matches page

--- Routing.php
+++ Routing.php
@@ -3,6 +3,7 @@
 require_once 'src/controllers/SecurityController.php';
 require_once 'src/controllers/DashboardController.php';
 require_once 'src/controllers/DiscoverController.php';
+require_once 'src/controllers/MatchesController.php';
 
 // TODO musimy zapewnic, ze utworzony 
 // obiekt kontrollera ma tylko jedna instancję - SINGLETON
@@ -55,6 +56,10 @@ class Routing {
                 break;
             case 'discover':
                 $controller = new DiscoverController();
+                $controller->index();
+                break;
+            case 'matches':
+                $controller = new MatchesController();
                 $controller->index();
                 break;
             default:
---
+++ src/controllers/MatchesController.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'AppController.php';
+
+class MatchesController extends AppController {
+
+    public function index() {
+        session_start();
+
+        if (!isset($_SESSION['user_id'])) {
+            $url = "http://$_SERVER[HTTP_HOST]";
+            header("Location: {$url}/login");
+            exit();
+        }
+
+        return $this->render('matches', [
+            'userEmail'  => $_SESSION['user_email'],
+            'activePage' => 'matches'
+        ]);
+    }
+}
