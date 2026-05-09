7e34d3bdfd1ceae7c889abe609fe643615784217
SewerynSatro <sewerynsatro@gmail.com>
2026-05-09 07:57:53 +0200

    Add dashboard layout with sidebar and session guard

--- src/controllers/DashboardController.php
+++ src/controllers/DashboardController.php
@@ -6,12 +6,18 @@ require_once __DIR__.'/../repositories/UsersRepository.php';
 class DashboardController extends AppController {
 
     public function index() {
-        // TODO pobieranie danych z bazy
-        // wstawianie zmiennych na widok
+        session_start();
+
+        if (!isset($_SESSION['user_id'])) {
+            $url = "http://$_SERVER[HTTP_HOST]";
+            header("Location: {$url}/login");
+            exit();
+        }
+
         $title = "INDEX";
         $usersRepository = new UsersRepository();
         $users = $usersRepository->getUsers();
 
-        return $this->render("index", ["title" => $title, "users" => $users]);
+        return $this->render("dashboard", ["title" => $title, "users" => $users]);
     }
 }
